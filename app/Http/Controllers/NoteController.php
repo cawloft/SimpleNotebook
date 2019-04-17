018fee665bb873c8ad4877953b9d697f061a90b4
cawloft <caw@cawloft.com>
2019-04-17 22:04:21 +0800

    some clean-ups on authentication

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller{
 
+    public function __construct()
+    {
+        $this->middleware('auth')->except([
+            'index', 'show'
+        ]);
+    }
+
     public function index(){
 
         $notes = Note::all();
